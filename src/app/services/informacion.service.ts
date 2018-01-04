cb14f29b7c6d58d13ec59e576709bc0c952c6516
lucas suarez <lucas.suarez.utepsa@gmail.com>
2018-01-04 14:24:56 -0400

    Servicio de firebase listo equipo de desarrolo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,14 +6,32 @@ export class InformacionService {
 
   info:any={};
   loading:boolean = false;
+  loading_about:boolean = false;
+  equipo:any[] = [];
 
   constructor(public http:Http) {
-    http.get('assets/data/info.pagina.json')
+
+    this.carga_info();
+    this.carga_sobre_nosotros();
+
+  }
+
+  public carga_info(){
+    this.http.get('assets/data/info.pagina.json')
       .subscribe(value=>{
         this.loading = true;
-        console.log(value.json());
         this.info = value.json();
       });
+  }
+
+  public carga_sobre_nosotros(){
+    this.http.get('https://templatepaginaweb.firebaseio.com/equipo.json')
+      .subscribe(value=>{
+        console.log(value.json())
+        this.loading_about = true;
+        this.equipo = value.json();
+      });
+
   }
 
 }
